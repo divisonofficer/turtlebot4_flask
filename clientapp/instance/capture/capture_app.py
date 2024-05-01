443c1b1864638da036658739261b52247e5c33e4
divionofficer <wlssud0701@naver.com>
2024-05-01 15:51:47 +0900

    [05.01] Capture Scenario - Space initialize - Load - Save

--- clientapp/instance/capture/capture_app.py
+++ clientapp/instance/capture/capture_app.py
@@ -3,9 +3,11 @@ import rclpy
 from capture_diagnostic import CaptureDiagnostic
 from capture_ros import CaptureNode
 from capture_storage import CaptureStorage
+from flask_socketio import SocketIO
+import cv2
 
 app = Flask(__name__)
-
+socketIO = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 capture_node: CaptureNode
 diag_node: CaptureDiagnostic
@@ -15,15 +17,49 @@ capture_storage = CaptureStorage()
 with app.app_context():
     rclpy.init()
     diag_node = CaptureDiagnostic()
-    capture_node = CaptureNode(capture_storage)
+    capture_node = CaptureNode(capture_storage, socketIO)
+
+
+@socketIO.on("connect", namespace="/socket")
+def connectSocket():
+    print("Connected to socket")
+
+
+@app.route("/init", methods=["POST"])
+def init_space():
+    space_id = request.json.get("space_id")
+    space_name = request.json.get("space_name")
+    result = capture_node.init_space(
+        int(space_id) if space_id else None, space_name if not space_id else None
+    )
+
+    if not result:
+        return Response(status=500)
+
+    if result["status"] == "error":
+        return Response(status=400, response=result)
+
+    return result
+
+
+@app.route("/close", methods=["POST"])
+def close_space():
+    result = capture_node.empty_space()
+    if not result:
+        return Response(status=500)
+    return result
+
+
+@app.route("/")
+def get_capture_status():
+    return {
+        "space_ready": capture_node.space_id is not None,
+        "space_id": capture_node.space_id,
+    }
 
 
 @app.route("/capture", methods=["POST"])
 def capture_at():
-
-    if capture_node.flag_capture_running:
-        return {"status": "error", "message": "Capture is already running"}
-
     capture_topic = None
     if request.json is not None:
         capture_topic = request.json.get("capture_topic")
@@ -32,14 +68,16 @@ def capture_at():
     else:
         capture_node.image_topics = ["/oakd/rgb/preview/image_raw"]
 
-    return capture_node.run_capture_queue_thread()
+    result = capture_node.run_capture_queue_thread()
+    if not result:
+        return Response(status=500)
+    if result["status"] == "error":
+        return Response(status=400, response=result)
+    return result
 
 
 @app.route("/capture/single", methods=["POST"])
 def capture_single():
-    if capture_node.flag_capture_running:
-        return {"status": "error", "message": "Capture is already running"}
-
     capture_topic = None
     if request.json is not None:
         capture_topic = request.json.get("capture_topic")
@@ -50,6 +88,8 @@ def capture_single():
     result = capture_node.run_capture_queue_single()
     if not result:
         return Response(status=500)
+    if result["status"] == "error":
+        return Response(status=400, response=result)
     return result
 
 
@@ -61,6 +101,11 @@ def capture_abort():
     capture_node.flag_abort = True
 
     return {"status": "success", "message": "Capture is aborted"}
+
+
+@app.route("/result", methods=["GET"])
+def capture_result_space_list():
+    return capture_storage.get_all_spaces()
 
 
 @app.route("/result/<space_id>")
@@ -92,5 +137,19 @@ def capture_result_image(space_id, capture_id, scene_id, filename):
     )
 
 
+@app.route(
+    "/result/<space_id>/<capture_id>/<scene_id>/<filename>/thumb", methods=["GET"]
+)
+def capture_result_image_thumb(space_id, capture_id, scene_id, filename):
+    img = cv2.imread(
+        capture_storage.get_capture_scene(
+            int(space_id), int(capture_id), int(scene_id), filename
+        )
+    )
+    img = cv2.resize(img, (128, 128))
+    _, img_encoded = cv2.imencode(".png", img)
+    return Response(img_encoded.tobytes(), mimetype="image/jpeg")
+
+
 if __name__ == "__main__":
-    app.run(port=5012)
+    socketIO.run(app, port=5012, allow_unsafe_werkzeug=True, host="0.0.0.0")
---
+++ clientapp/instance/capture/slam_source.py
@@ -0,0 +1,48 @@
+import requests
+
+
+class SlamSource:
+    SLAM_URL = "http://localhost/slam/"
+
+    def __init__(self):
+        pass
+
+    def search_map_name_empty(self, map_name: str):
+        response = requests.get(f"{self.SLAM_URL}map/{map_name}")
+        if response.status_code == 200:
+            return False
+        return True
+
+    def request_map_save(self, map_name: str):
+        """
+        return 0 if success
+        return message if failed
+        """
+        response = requests.post(
+            f"{self.SLAM_URL}map/save", json={"filename": map_name}
+        )
+        if response.status_code == 200:
+            return None
+        return response
+
+    def request_map_save_and_clean(self, map_name: str):
+        with self.request_map_save(map_name) as response:
+            if response:
+                return response
+
+        response = requests.get(f"{self.SLAM_URL}cancel")
+        if response.status_code == 200:
+            return None
+        return response.status_code
+
+    def request_map_load(self, map_name: str):
+        """
+        return 0 if success
+        return message if failed
+        """
+        response = requests.post(
+            f"{self.SLAM_URL}map/load", json={"filename": map_name}
+        )
+        if response.status_code == 200:
+            return None
+        return response.message
