9e2016bd7fca1a07e09c4f7dbc9fe1fdf5dece24
Jinnyeong.Kim <wlssud0701@naver.com>
2024-06-07 17:50:47 +0900

    [06.07] 1. Polarimetirc Page Store 2. By pixel color viewer for polarimetry 3. PageRoot Width Adjust

--- reactapp/src/page/polarization/PolarizationCalibration.tsx
+++ reactapp/src/page/polarization/PolarizationCalibration.tsx
@@ -5,9 +5,11 @@ import {
   Button,
   Flex,
   HStack,
+  IconButton,
   Input,
   Menu,
   MenuButton,
+  MenuGroup,
   MenuItem,
   MenuList,
   Slider,
@@ -16,49 +18,169 @@ import {
   SliderTrack,
   VStack,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import {
-  CaptureAppCapture,
-  CaptureAppScene,
-  CaptureAppSpace,
-} from "../../public/proto/capture";
+import { useEffect, useRef, useState } from "react";
 import { Body3, H4 } from "../../design/text/textsystem";
-import { httpGet } from "../../connect/http/request";
+import { ArrowBendDoubleUpLeft } from "@phosphor-icons/react";
+import {
+  CHANNEL,
+  PROPERTIES,
+  polarizationStore,
+} from "../../stores/PolarizationStore";
+
+export const PolarizationView = observer((props: { randomTime: number }) => {
+  return (
+    <VStack style={{ width: "100%" }}>
+      {PROPERTIES.map((property, index_sub) => (
+        <HStack
+          style={{
+            width: "90%",
+            overflowX: "auto",
+          }}
+        >
+          {CHANNEL.map((channel, index) => (
+            <VStack
+              flexGrow="1"
+              onClick={() => {
+                polarizationStore.viewFocusProperty = `${channel}_${property}`;
+              }}
+            >
+              <img
+                src={`/capture/polarization/view/${channel}_${property}?timestamp=${
+                  new Date().getTime() + props.randomTime
+                }`}
+                style={{ minWidth: "30rem", flexGrow: "1", height: "auto" }}
+                alt={property}
+              />
+              <Body3>
+                {channel}_{property}
+              </Body3>
+            </VStack>
+          ))}{" "}
+        </HStack>
+      ))}
+    </VStack>
+  );
+});
+
+export const ConfigUpdateSlider = observer(
+  (props: {
+    title: string;
+    value: number;
+    range: [number, number];
+    step: number;
+    onChange: (value: number, onSuccess: () => void) => void;
+  }) => {
+    const [valueDisplay, setValueDisplay] = useState(props.value);
+
+    useEffect(() => {
+      setValueDisplay(props.value);
+    }, [props.value]);
+
+    return (
+      <VStack>
+        <H4>{props.title}</H4>
+        <Slider
+          min={props.range[0]}
+          max={props.range[1]}
+          size={"md"}
+          style={{
+            width: "100%",
+
+            height: "1rem",
+          }}
+          step={props.step}
+          value={props.value}
+          onChange={(value) => {
+            props.onChange(value, () => {
+              setValueDisplay(value);
+            });
+          }}
+        >
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+        <Input
+          type="number"
+          value={valueDisplay}
+          onChange={(e) => setValueDisplay(parseFloat(e.target.value))}
+          onKeyDownCapture={(e) => {
+            if (e.key === "Enter") {
+              props.onChange(valueDisplay, () => {});
+            }
+          }}
+        />
+      </VStack>
+    );
+  }
+);
 
 export const PolarizationLinearCalibration = observer(() => {
-  const [angle, setAngle] = useState(0);
-  const [qwpAngle, setQwpAngle] = useState(0);
-  const [capture, setCapture] = useState<CaptureAppCapture | undefined>(
-    undefined
-  );
-  const [space, setSpace] = useState<CaptureAppSpace | undefined>(undefined);
-  const [scene, setScene] = useState<CaptureAppScene | undefined>(undefined);
-
-  const [viewFocusProperty, setViewFocusProperty] = useState<
-    string | undefined
-  >(undefined);
-
   useEffect(() => {
     captureStore.fetchGetCaptureSpaceList();
   }, []);
 
-  return (
-    <VStack>
+  const PolarizationAngleConfig = () => {
+    return (
+      <HStack>
+        <ConfigUpdateSlider
+          title="Linear Polarizer Angle"
+          value={polarizationStore.mmLinearAngle}
+          range={[-45, 45]}
+          step={0.0001}
+          onChange={(value, onSuccess) => {
+            polarizationStore.fetchComputePolarization({
+              angle: value,
+              doOnSuccess: () => {
+                polarizationStore.mmLinearAngle = value;
+                onSuccess();
+              },
+            });
+          }}
+        />
+
+        <ConfigUpdateSlider
+          title="QWP Polarizer Angle"
+          value={polarizationStore.mmQwpAngle}
+          range={[-45, 45]}
+          step={0.0001}
+          onChange={(value, onSuccess) => {
+            polarizationStore.fetchComputePolarization({
+              qwpAngle: value,
+              doOnSuccess: () => {
+                polarizationStore.mmQwpAngle = value;
+                onSuccess();
+              },
+            });
+          }}
+        />
+      </HStack>
+    );
+  };
+
+  const PolarizationSceneMenu = observer(() => {
+    return (
       <HStack
         style={{
           width: "100%",
           justifyContent: "flex-start",
         }}
       >
-        <Menu placement="bottom-start">
+        <Menu
+          placement="bottom-start"
+          onOpen={() => {
+            captureStore.fetchGetCaptureSpaceList();
+          }}
+        >
           <MenuButton as={Button} size="sm">
-            {space?.spaceName || "Space"}
+            {polarizationStore.space?.spaceName || "Space"}
           </MenuButton>
           <MenuList maxHeight={"20rem"} overflowY="scroll">
             {captureStore.captureSpaceList.map((space, index) => (
               <MenuItem
                 key={index}
-                onClick={() => setSpace(space)}
+                onClick={() => (polarizationStore.space = space)}
                 fontSize={"small"}
               >
                 {space.spaceName} / {space.captures.length}
@@ -67,16 +189,16 @@ export const PolarizationLinearCalibration = observer(() => {
           </MenuList>
         </Menu>
 
-        {space && (
+        {polarizationStore.space && (
           <Menu placement="bottom-start">
             <MenuButton as={Button} size="sm">
-              {capture?.captureId || "capture"}
+              {polarizationStore.capture?.captureId || "capture"}
             </MenuButton>
             <MenuList maxHeight={"20rem"} overflowY="scroll">
-              {space.captures.map((capture, index) => (
+              {polarizationStore.space.captures.map((capture, index) => (
                 <MenuItem
                   key={index}
-                  onClick={() => setCapture(capture)}
+                  onClick={() => (polarizationStore.capture = capture)}
                   fontSize="small"
                 >
                   {capture.captureId} / {capture.scenes.length}
@@ -85,29 +207,28 @@ export const PolarizationLinearCalibration = observer(() => {
             </MenuList>
           </Menu>
         )}
-        {capture && (
+        {polarizationStore.capture && (
           <Menu>
             <MenuButton as={Button} size="sm">
-              {scene
-                ? scene.images
+              {polarizationStore.scene
+                ? polarizationStore.scene.images
                     .filter((image) => image.includes("left_channel_0"))
                     .map((image) => image.split(".png")[0].split("_").pop())
                     .join(", ")
                 : "scene"}
             </MenuButton>
             <MenuList overflowY="scroll" maxHeight={"20rem"}>
-              {capture.scenes.map((scene, index) => (
+              {polarizationStore.capture.scenes.map((scene, index) => (
                 <MenuItem
                   fontSize={"small"}
                   key={index}
                   onClick={() =>
-                    httpGet(`
-                /capture/polarization/linear/${space?.spaceId}/${capture?.captureId}/${scene.sceneId}/${angle}/${qwpAngle}
-              `)
-                      .onSuccess((data) => {
-                        setScene(scene);
-                      })
-                      .fetch()
+                    polarizationStore.fetchComputePolarization({
+                      sceneId: scene.sceneId,
+                      doOnSuccess: () => {
+                        polarizationStore.scene = scene;
+                      },
+                    })
                   }
                 >
                   {index} __
@@ -121,174 +242,121 @@ export const PolarizationLinearCalibration = observer(() => {
           </Menu>
         )}
 
-        {scene && (
+        {polarizationStore.scene && (
           <Menu>
             <MenuButton as={Button} size="sm">
-              {viewFocusProperty || "ViewProperty"}
+              {polarizationStore.viewFocusProperty || "ViewProperty"}
             </MenuButton>
-            <MenuList>
-              {[
-                [
-                  "s0_rgb_positive",
-                  "s1_rgb_positive",
-                  "s2_rgb_positive",
-                  "s3_rgb_positive",
-                ],
-                [
-                  "s0_rgb_negative",
-                  "s1_rgb_negative",
-                  "s2_rgb_negative",
-                  "s3_rgb_negative",
-                ],
-                [
-                  "s0_nir_positive",
-                  "s1_nir_positive",
-                  "s2_nir_positive",
-                  "s3_nir_positive",
-                ],
-                [
-                  "s0_nir_negative",
-                  "s1_nir_negative",
-                  "s2_nir_negative",
-                  "s3_nir_negative",
-                ],
-                ["r_aolp", "g_aolp", "b_aolp", "nir_aolp"],
-                ["r_dolp", "g_dolp", "b_dolp", "nir_dolp"],
-              ].map((property_group, index) =>
-                property_group.map((property, index_sub) => (
-                  <MenuItem
-                    onClick={() => setViewFocusProperty(property)}
-                    fontSize="small"
-                  >
-                    {property}
-                  </MenuItem>
-                ))
-              )}
+            <MenuList overflowY="scroll" maxHeight={"20rem"}>
+              <MenuItem
+                onClick={() =>
+                  (polarizationStore.viewFocusProperty = `rgb_s0_positive`)
+                }
+                fontSize="small"
+              >
+                RGB_S0
+              </MenuItem>
+              {CHANNEL.map((channel, index) => (
+                <MenuGroup title={channel}>
+                  {PROPERTIES.map((property, index_sub) => (
+                    <MenuItem
+                      onClick={() =>
+                        (polarizationStore.viewFocusProperty = `${channel}_${property}`)
+                      }
+                      fontSize="small"
+                    >
+                      {property}
+                    </MenuItem>
+                  ))}
+                </MenuGroup>
+              ))}
             </MenuList>
           </Menu>
         )}
-      </HStack>
-
-      {viewFocusProperty && (
-        <img
-          src={`/capture/polarization/view/${viewFocusProperty}?timestamp=${new Date().getTime()}`}
-          style={{ width: "80%", height: "auto" }}
-          alt="viewFocusProperty"
+        <IconButton
+          aria-label="Refresh"
+          icon={<ArrowBendDoubleUpLeft />}
+          onClick={() => {
+            polarizationStore.fetchComputePolarization();
+          }}
         />
-      )}
-      <HStack>
-        <VStack>
-          <H4>Linear Polarizer Angle Adjust</H4>
-          <Slider
-            min={-90}
-            max={90}
-            size={"md"}
-            style={{
-              width: "100%",
-
-              height: "1rem",
-            }}
-            step={0.0001}
-            value={angle}
-            onChange={(value) => {
-              httpGet(`
-            /capture/polarization/linear/${space?.spaceId}/${capture?.captureId}/${scene?.sceneId}/${value}/${qwpAngle}
-          `)
-                .onSuccess((data) => {
-                  setAngle(value);
-                })
-                .fetch();
-            }}
-          >
-            <SliderTrack>
-              <SliderFilledTrack />
-            </SliderTrack>
-            <SliderThumb />
-          </Slider>
-          <Input
-            type="number"
-            value={angle}
-            onChange={(e) => setAngle(parseFloat(e.target.value))}
-          />
-        </VStack>
-        <VStack>
-          <H4>QWP Polarizer Angle Adjust</H4>
-          <Slider
-            min={-90}
-            max={90}
-            size={"md"}
-            style={{
-              width: "100%",
-
-              height: "1rem",
-            }}
-            step={0.0001}
-            value={qwpAngle}
-            onChange={(value) => {
-              httpGet(`
-            /capture/polarization/linear/${space?.spaceId}/${capture?.captureId}/${scene?.sceneId}/${angle}/${value}
-          `)
-                .onSuccess((data) => {
-                  setQwpAngle(value);
-                })
-                .fetch();
-            }}
-          >
-            <SliderTrack>
-              <SliderFilledTrack />
-            </SliderTrack>
-            <SliderThumb />
-          </Slider>
-          <Input
-            type="number"
-            value={qwpAngle}
-            onChange={(e) => setQwpAngle(parseFloat(e.target.value))}
-          />
-        </VStack>
       </HStack>
+    );
+  });
 
-      {[
-        [
-          "s0_rgb_positive",
-          "s1_rgb_positive",
-          "s2_rgb_positive",
-          "s3_rgb_positive",
-        ],
-        [
-          "s0_rgb_negative",
-          "s1_rgb_negative",
-          "s2_rgb_negative",
-          "s3_rgb_negative",
-        ],
-        [
-          "s0_nir_positive",
-          "s1_nir_positive",
-          "s2_nir_positive",
-          "s3_nir_positive",
-        ],
-        [
-          "s0_nir_negative",
-          "s1_nir_negative",
-          "s2_nir_negative",
-          "s3_nir_negative",
-        ],
-        ["r_aolp", "g_aolp", "b_aolp", "nir_aolp"],
-        ["r_dolp", "g_dolp", "b_dolp", "nir_dolp"],
-      ].map((property_group, index) => (
-        <Flex wrap="wrap" width="100%">
-          {property_group.map((property, index_sub) => (
-            <VStack>
-              <img
-                src={`/capture/polarization/view/${property}?timestamp=${new Date().getTime()}`}
-                style={{ width: "20rem", height: "12rem" }}
-                alt={property}
-              />
-              <Body3>{property}</Body3>
-            </VStack>
-          ))}{" "}
-        </Flex>
-      ))}
+  return (
+    <VStack
+      style={{
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <PolarizationSceneMenu />
+      <PolarizationAngleConfig />
+      <VStack
+        style={{
+          width: "100%",
+          overflowY: "auto",
+        }}
+      >
+        {polarizationStore.viewFocusProperty && <PolarizationDeepView />}
+
+        <PolarizationView randomTime={polarizationStore.randomTime} />
+      </VStack>
     </VStack>
+  );
+});
+
+export const PolarizationDeepView = observer(() => {
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  return (
+    <Flex
+      style={{
+        position: "relative",
+      }}
+    >
+      <img
+        ref={imageRef}
+        src={`/capture/polarization/view/${
+          polarizationStore.viewFocusProperty
+        }?timestamp=${new Date().getTime() + polarizationStore.randomTime}`}
+        style={{ width: "80%", height: "auto" }}
+        alt="viewFocusProperty"
+        onMouseMove={(e) => {
+          imageRef.current &&
+            polarizationStore.fetchColorDeepView(
+              e.nativeEvent.offsetY / imageRef.current.clientHeight,
+              e.nativeEvent.offsetX / imageRef.current.clientWidth
+            );
+        }}
+      />
+      {polarizationStore.colorDeepView && (
+        <VStack
+          style={{
+            position: "absolute",
+            top: polarizationStore.colorDeepView.y * 100 + "%",
+            left: polarizationStore.colorDeepView.x * 100 + "%",
+            background: "#AAFFFFFF",
+          }}
+        >
+          <Body3>
+            {polarizationStore.colorDeepView.x.toFixed(4)} /{" "}
+            {polarizationStore.colorDeepView.y.toFixed(4)}
+          </Body3>
+          <HStack>
+            {polarizationStore.colorDeepView.colorList &&
+              Object.entries(polarizationStore.colorDeepView.colorList).map(
+                (entry, index) => (
+                  <Body3 key={index}>
+                    {entry[0]}:{entry[1].toFixed(3)}
+                  </Body3>
+                )
+              )}
+          </HStack>
+        </VStack>
+      )}
+    </Flex>
   );
 });
 
---
+++ reactapp/src/stores/PolarizationStore.ts
@@ -0,0 +1,86 @@
+import { action, makeAutoObservable } from "mobx";
+import {
+  CaptureAppCapture,
+  CaptureAppScene,
+  CaptureAppSpace,
+} from "../public/proto/capture";
+import { httpGet } from "../connect/http/request";
+
+export const PROPERTIES = [
+  "s0_combined",
+  "s1_combined",
+  "s2_combined",
+  "s3_combined",
+  "aolp",
+  "dolp",
+  "dop",
+  "docp",
+  "diffuse",
+  "ell_angle",
+  "specular",
+];
+
+export const CHANNEL = ["r", "g", "b", "nir"];
+
+class PolarizationStore {
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  mmLinearAngle = 0;
+  mmQwpAngle = 0;
+  capture?: CaptureAppCapture = undefined;
+  space?: CaptureAppSpace = undefined;
+  scene?: CaptureAppScene = undefined;
+  viewFocusProperty?: string = undefined;
+  randomTime = 0;
+  colorDeepView?: {
+    x: number;
+    y: number;
+    colorList: { [key: string]: number };
+  } = undefined;
+
+  @action
+  fetchComputePolarization(
+    props: {
+      spaceId?: number;
+      captureId?: number;
+      sceneId?: number;
+      angle?: number;
+      qwpAngle?: number;
+      doOnSuccess?: () => void;
+    } = {}
+  ) {
+    console.log(props);
+    const spaceId = props.spaceId || this.space?.spaceId;
+    const captureId = props.captureId || this.capture?.captureId;
+    const sceneId =
+      props.sceneId !== undefined ? props.sceneId : this.scene?.sceneId;
+    const linearAngle = props.angle || this.mmLinearAngle;
+    const qwpPlateAngle = props.qwpAngle || this.mmQwpAngle;
+
+    httpGet(`
+      /capture/polarization/linear/${spaceId}/${captureId}/${sceneId}/${linearAngle}/${qwpPlateAngle}
+    `)
+      .onSuccess((data) => {
+        props.doOnSuccess && props.doOnSuccess();
+        this.randomTime = new Date().getTime();
+      })
+      .fetch();
+  }
+
+  @action
+  fetchColorDeepView(y: number, x: number) {
+    httpGet(`/capture/polarization/view/${this.viewFocusProperty}/${y}/${x}`)
+      .onSuccess((data) => {
+        this.colorDeepView = {
+          x,
+          y,
+          colorList: data,
+        };
+      })
+      .fetch();
+  }
+}
+
+export const polarizationStore = new PolarizationStore();
