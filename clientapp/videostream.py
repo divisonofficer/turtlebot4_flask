94a91cb150acf770764d03a5e4e0c2047b7e9443
divionofficer <wlssud0701@naver.com>
2024-07-23 14:53:24 +0900

    [07.23] VideoStream Process Trigger

--- clientapp/videostream.py
+++ clientapp/videostream.py
@@ -53,25 +53,37 @@ class VideoStream:
     cv_image = None
     interval_callback = None
     lock = threading.Lock()
+    generator_count = 0
 
     def cv_raw_callback(self, msg: Union[Image, CompressedImage]):
-
+        if self.generator_count < 1:
+            return
         if isinstance(msg, CompressedImage):
             np_arr = np.frombuffer(msg.data, np.uint8)
             frame_format = ""
             frame_bits = 8
-            if "bit" in msg.header.frame_id:
+            print(msg.header)
+            print(np_arr.shape, np_arr.dtype)
+            if "bit" in msg.header.frame_id and not "8bit" in msg.header.frame_id:
                 cv_image = (decode_jai_compressedImage(msg) / 256).astype(np.uint8)
 
                 if frame_format == "bayer":
                     cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BayerBG2BGR)
+            elif np_arr.shape[0] % (1440 * 1080) == 0:
+                cv_image = np.reshape(
+                    np_arr, (1080, 1440, np_arr.shape[0] // (1440 * 1080))
+                ).astype(np.uint8)
+                if cv_image.shape[2] == 3:
+                    cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
             else:
-                cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+                cv_image = cv2.imdecode(np_arr, cv2.IMREAD_ANYCOLOR)
 
         else:
             cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
             if msg.encoding == "bayer_rggb8":
                 cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BayerBG2BGR)
+        if cv_image is None:
+            return
         self.cv_image = cv_image.copy()
         if self.timestampWatermark:
             time_text = time.strftime("%H:%M:%S", time.localtime(msg.header.stamp.sec))
@@ -104,33 +116,39 @@ class VideoStream:
     def generate_preview(self, timeStamp: Union[str, None] = None, isGrayScale=False):
         # if timeStamp:
         #     self.running[timeStamp] = True
-        while True:
-            # if not self.running[timeStamp]:
-            #     break
-            with self.lock:
-                if self.output_frame is None:
-                    continue
-                self.topic_time_interval = time.time() - self.topic_time
-                self.topic_time = time.time()
-                if isGrayScale:
-                    self.output_frame = cv2.cvtColor(
-                        self.output_frame, cv2.COLOR_BGR2GRAY
+        self.generator_count += 1
+        try:
+            while True:
+                # if not self.running[timeStamp]:
+                #     break
+                with self.lock:
+                    if self.output_frame is None:
+                        continue
+                    self.topic_time_interval = time.time() - self.topic_time
+                    self.topic_time = time.time()
+                    if isGrayScale:
+                        self.output_frame = cv2.cvtColor(
+                            self.output_frame, cv2.COLOR_BGR2GRAY
+                        )
+
+                    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
+                    (flag, encodedImage) = cv2.imencode(
+                        ".jpg", self.output_frame, encode_param
                     )
 
-                encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-                (flag, encodedImage) = cv2.imencode(
-                    ".jpg", self.output_frame, encode_param
+                yield (
+                    b"--frame\r\n"
+                    b"Content-Type: image/jpeg\r\n\r\n"
+                    + bytearray(encodedImage)
+                    + b"\r\n"
                 )
 
-            yield (
-                b"--frame\r\n"
-                b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encodedImage) + b"\r\n"
-            )
+                self.yield_time_interval = time.time() - self.yield_time
+                self.yield_time = time.time()
 
-            self.yield_time_interval = time.time() - self.yield_time
-            self.yield_time = time.time()
-
-            if self.interval_callback:
-                self.interval_callback(
-                    self.topic_time_interval, self.yield_time_interval
-                )
+                if self.interval_callback:
+                    self.interval_callback(
+                        self.topic_time_interval, self.yield_time_interval
+                    )
+        except GeneratorExit:
+            self.generator_count -= 1
