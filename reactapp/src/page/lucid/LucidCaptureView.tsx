dc30b3a9be4b6467a01c1769b8edb177d219918a
Jinnyeong.Kim <wlssud0701@naver.com>
2024-08-22 15:15:40 +0900

    [08.22] Status Button Abstract

--- reactapp/src/page/lucid/LucidCaptureView.tsx
+++ reactapp/src/page/lucid/LucidCaptureView.tsx
@@ -3,7 +3,7 @@ import { PageRoot } from "../../design/other/flexs";
 import { lucidStore } from "../../stores/LucidStore";
 import { Btn } from "../../design/button/button";
 import { Body3 } from "../../design/text/textsystem";
-import { VStack } from "@chakra-ui/react";
+import { Switch, VStack } from "@chakra-ui/react";
 
 export const LucidCaptureView = observer(() => {
   return (
@@ -12,6 +12,16 @@ export const LucidCaptureView = observer(() => {
         <Btn onClick={lucidStore.fetchDisableStorage}>Disable Storage</Btn>
       ) : (
         <Btn onClick={lucidStore.fetchEnableStorage}>Enable Storage</Btn>
+      )}
+      {lucidStore.lucidStatus.hasOwnProperty("single_storage_mode") && (
+        <Switch
+          isChecked={lucidStore.lucidStatus["single_storage_mode"]}
+          onChange={(e) => {
+            lucidStore.fetchUpdateStatusAttributes({
+              single_storage_mode: e.target.checked,
+            });
+          }}
+        />
       )}
 
       <VStack
